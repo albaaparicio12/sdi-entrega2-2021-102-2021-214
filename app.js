70f05f95a30c495daf373d5771e9578c22aecda8
Álvaro <alvaro.tejido18@gmail.com>
2021-04-28 12:20:13 +0200

    Casi toda la lógica del caso 2, 3 y 4

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
 //Módulos
 let express = require('express');
-let swig = require('swig');
 let app = express();
 
+var expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+let swig = require('swig');
+let crypto = require('crypto');
+let mongo = require('mongodb');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
+// routerUsuarioSession
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function(req, res, next) {
+    console.log("routerUsuarioSession");
+    if ( req.session.usuario ) {
+        // dejamos correr la petición
+        next();
+    } else {
+        console.log("va a : "+req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+
+//Aplicar routerUsuarioSession
+//app.use("/canciones/agregar",routerUsuarioSession);
 
 app.use(express.static('public'));
 
 //Variables
 app.set('port', 8081);
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
+//AQUI TIENE QUE IR app.set('db', "...")
 
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 
 app.listen(app.get('port'), function() {
     console.log('Servidor activo');
--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -1,4 +1,4 @@
-module.exports = function(app, swig) {
+module.exports = function(app, swig, gestorBD) {
     app.get("/usuario/add", function(req, res) {
         let respuesta = swig.renderFile('views/bregistro.html',{});
         res.send(respuesta);
@@ -11,15 +11,69 @@ module.exports = function(app, swig) {
             nombre : req.body.nombre,
             apellidos : req.body.apellidos,
             email : req.body.email,
-            repetirPassword : req.body.repetirPassword
+            repetirPassword : req.body.repetirPassword,
+            dinero : 100,
+            rol : "estandar"
         }
+        if(req.body.password != req.body.repetirPassword){
+            res.redirect("add?mensaje=Error con las passwords, no coinciden")
+        } else{
+            res.send(swig.renderFile('views/opciones.html'));
 
- //       gestorBD.insertarUsuario(usuario, function(id){
- //           if(id == null){
- //               res.redirect("usuario/add?mensaje=Error al registrar al usuario");
- //           } else {
- //               res.redirect("usuarios");
- //           }
- //       })
+                   gestorBD.insertarUsuario(usuario, function(id){
+                       if(id == null){
+                           res.redirect("usuario/add?mensaje=Error al registrar al usuario");
+                       } else {
+                           res.redirect("usuarios");
+                       }
+                   })
+        }
     })
+
+    app.get("/identificarse", function(req, res) {
+        let respuesta = swig.renderFile('views/bidentificacion.html');
+        res.send(respuesta);
+    });
+
+    app.post("/identificarse", function(req, res) {
+        let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        let criterio = {
+            email : req.body.email,
+            password : seguro
+        }
+        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                req.session.usuario = null;
+                res.send("No identificado: ");
+            } else {
+                req.session.usuario = usuarios[0].email;
+                if(req.session.usuario == "admin@email.com"){
+                    res.send(swig.renderFile('views/opcionesAdmin.html'))
+                } else {
+                    res.send(swig.renderFile('views/opciones.html'));
+                }
+            }
+        });
+    });
+
+    app.get('/desconectarse', function (req, res) {
+        req.session.usuario = null;
+        res.send('views/bregistro.html');
+    })
+
+    app.get('/usuario/listado', function (req, res){
+        let criterio = {};
+        gestorBD.obtenerUsuarios(criterio, function(lista){
+            if(lista == null){
+                res.send("Error al listar");
+            } else {
+                let respuesta = swig.renderFile('views/listado.html',
+                    {
+                        listado : lista
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
 };
